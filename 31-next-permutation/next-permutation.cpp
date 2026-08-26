5b9ea24b10b064bcf804130d80998ed5939d1bbd
Monikapm19 <monikapm727@gmail.com>
2026-08-26 10:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (54.62%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        //find the pivot
+        int pivot=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                pivot=i;
+                break;
+                
+            }
+        }
+        if(pivot==-1){
+                reverse(nums.begin(),nums.end());
+                return;
+
+            }
+        //finding the rightmost element graeter tham pivot
+        for(int i=n-1;i>pivot;i--){
+            if(nums[i]>nums[pivot]){
+                swap(nums[i],nums[pivot]);
+                break;
+            }
+        }
+        //reverse from pivot+1 to n-1;
+        int i=pivot+1;
+        int j=n-1;
+        while(i<=j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+    }
+};
